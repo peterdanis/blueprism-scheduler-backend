0f7003400426a826a134d2fbb3f730220a177fec
Peter Danis <peter.danis@outlook.com>
2021-01-12 12:54:45 +0100

    Enhance runtimeresource entity

--- src/entity/RuntimeResource.ts
+++ src/entity/RuntimeResource.ts
@@ -3,13 +3,30 @@ import Base from "./Base";
 import Job from "./Job";
 import Schedule from "./Schedule";
 
+type Auth = "basic" | "apikey";
+
 @Entity()
 export default class RuntimeResource extends Base {
+  @Column({ nullable: true })
+  apiKey!: string;
+
   @Column()
-  hostname!: string;
+  auth!: Auth;
 
   @Column()
   friendlyName!: string;
+
+  @Column()
+  hostname!: string;
+
+  @Column({ nullable: true })
+  password!: string;
+
+  @Column()
+  port!: number;
+
+  @Column({ nullable: true })
+  username!: string;
 
   @OneToMany(() => Job, (job) => job.schedule)
   job!: Job[];
