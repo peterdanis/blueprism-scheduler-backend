e3f07ab3ee44d72ca4b47502b8655196a20a3bf3
Peter Danis <peter.danis@outlook.com>
2021-01-28 23:12:55 +0100

    WIP on tests

--- tests/controller/schedule.test.ts
+++ tests/controller/schedule.test.ts
@@ -4,7 +4,9 @@ import {
   registerExistingSchedules,
 } from "../../src/controller/schedule";
 import config from "../testUtils/connectionHelper";
+import { install } from "@sinonjs/fake-timers";
 import loadDummies from "../testUtils/loadDummies";
+import { scheduledJobs } from "node-schedule";
 
 let connection: Connection;
 
@@ -15,12 +17,19 @@ beforeAll(async () => {
 
 afterAll(async () => {
   await connection.close();
+  Object.values(scheduledJobs).forEach((job) => job.cancel());
 });
 
-describe("Init", () => {
-  test("Description", async () => {
+describe("Scheduler", () => {
+  test("will register valid schedue", async () => {
+    const clock = install({
+      now: new Date("2021-01-20T02:00:00Z"),
+    });
+
     await registerExistingSchedules();
     const schedule = getScheduleRef(1);
-    expect(schedule?.name).toBeDefined();
+    // @ts-ignore
+    clock.tick("05:00");
+    expect(schedule?.name).toMatch("1");
   });
 });
