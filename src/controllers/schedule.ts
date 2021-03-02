a1b949cde74917489fab850a08dda14586381b04
Peter Danis <peter.danis@outlook.com>
2021-03-02 23:48:00 +0100

    Extend schedule controller

--- src/controllers/schedule.ts
+++ src/controllers/schedule.ts
@@ -3,6 +3,28 @@ import { addJob } from "./job";
 import Job from "../entities/Job";
 import Schedule from "../entities/Schedule";
 
+let schedulesCache: Schedule[];
+
+export const getSchedules = async (): Promise<Schedule[]> => {
+  if (!schedulesCache) {
+    schedulesCache = await Schedule.find();
+  }
+  return schedulesCache;
+};
+
+export const getSchedule = async (
+  nameOrId: string | number,
+): Promise<Schedule | undefined> => {
+  const schedules = await getSchedules();
+  const [schedule] = schedules.filter((_schedule) => {
+    if (typeof nameOrId === "string") {
+      return _schedule.name === nameOrId;
+    }
+    return _schedule.id === nameOrId;
+  });
+  return schedule;
+};
+
 // TODO: check start and end date/time before adding to jobs
 export const registerSchedule = (schedule: Schedule): void => {
   const { id, rule, validFrom, validUntil } = schedule;
