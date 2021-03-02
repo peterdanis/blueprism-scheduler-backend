0ee3c316c2e34ed8299e7262f26ec045f05b15d9
Peter Danis <peter.danis@outlook.com>
2021-03-02 21:25:55 +0100

    Extent user controller

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,51 @@
+import { addUser, getUser, getUsers } from "../controllers/user";
 import { Router } from "express";
+import User from "../entities/User";
 
 const router = Router();
 
-router.get();
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await getUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, password } = req.params;
+    if (!name || !password) {
+      res.status(422).json({
+        error: `User can not be created, ${
+          name ? "password" : "name"
+        } paremeter is missing`,
+      });
+      return;
+    }
+    const user = await addUser(name, password);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    let user: User | undefined;
+    if (userId && !Number.isNaN(userId)) {
+      user = await getUser(parseInt(userId, 10));
+    }
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
