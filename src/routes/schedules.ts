1ecefd8592a7bf646c99cea574b440269d29df3c
Peter Danis <peter.danis@outlook.com>
2021-03-11 14:18:38 +0100

    WIP on schedules route

--- src/routes/schedules.ts
+++ src/routes/schedules.ts
@@ -1,10 +1,13 @@
+import { getSchedules } from "../controllers/schedule";
 import { Router } from "express";
 
 const router = Router();
 
+// Get all schedules
 router.get("/", async (req, res, next) => {
   try {
-    //
+    const schedules = await getSchedules();
+    res.status(200).json(schedules);
   } catch (error) {
     next(error);
   }
--- tests/api.test.ts
+++ tests/api.test.ts
@@ -1,19 +1,33 @@
 import app from "../src/api";
 import request from "supertest";
+import Schedule from "../src/entities/Schedule";
 import User from "../src/entities/User";
-
-// User.find = jest.fn();
 
 const dummyUser: Partial<User> = {
   id: 1,
-  name: "test",
+  name: "TestUser",
+};
+
+const dummySchedule: Partial<Schedule> = {
+  id: 1,
+  name: "TestSchedule",
 };
 
 const mockUser = jest.spyOn(User, "find");
+jest.mock("../src/utils/logger");
 
 mockUser.mockImplementation(
   async (): Promise<User[]> => {
     return [dummyUser as User];
+  },
+);
+
+const mockSchedule = jest.spyOn(Schedule, "find");
+jest.mock("../src/utils/logger");
+
+mockSchedule.mockImplementation(
+  async (): Promise<Schedule[]> => {
+    return [dummySchedule as Schedule];
   },
 );
 
@@ -23,10 +37,17 @@ jest.mock("../src/entities/User.ts");
 
 // const post = (route, input) => request(app).post(route).send(input); // .auth(username, pw);
 
-describe("Description", () => {
-  test("Description", async () => {
+describe("Users route", () => {
+  test("can get all users", async () => {
     const result = await get("/api/users");
     expect(result.status).toBe(200);
     expect(result.body).toEqual([dummyUser]);
+  });
+});
+
+describe("Schedules route", () => {
+  test("can get all schedules", async () => {
+    const result = await get("/api/schedules");
+    expect(result.status).toBe(200);
   });
 });
